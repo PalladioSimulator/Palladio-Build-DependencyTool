4e36a9615010cb16fc5cc7585c1f9978a74238d3
Yves <10582363+YvesKirschner@users.noreply.github.com>
2021-05-19 10:19:47 +0200

    [PALLADIO-534] Write the results directly from to a database (#7)
    [PALLADIO-534] Write the results directly from to a database
    
    The results can now be stored in a Neo4j database directly after generation. For this purpose, only the command line parameter -neo4j must be specified when calling.
    * The -neo4j output flag is now used as expected.
    * The EmbeddedNeo4j class is now automatically closable to make it usable in a try block.
    * It is now checked whether the ./neo4j directory exists and a warning is printed.
    * To create an uber-jar that contained Neo4j as an embedded database, the maven-shade-plugin is now used instead of the maven-assembly-plugin.
    * Adding a help overview of the CLI parameters.
    * Adding a README.md

--- src/main/java/org/palladiosimulator/dependencytool/DependencyCLI.java
+++ src/main/java/org/palladiosimulator/dependencytool/DependencyCLI.java
@@ -14,6 +14,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.xml.sax.SAXException;
@@ -26,31 +27,40 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 /**
  * Command Line Interface for the dependency tool.
  */
-public class DependencyCLI {
-    
+public final class DependencyCLI {
+
     private static final Logger LOGGER = Logger.getLogger(DependencyCLI.class.getName());
-    
+
     /**
      * Main method for CLI.
-     * 
+     *
      * @param args organization and authentication-token are required arguments.
      * @throws ParseException
      */
     public static void main(String[] args) throws ParseException {
-        
-        Options options = createOptions();
-        
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse(options, args);
-        
-        String org = cmd.getOptionValue("o");
-        String token = cmd.getOptionValue("at");
-        boolean includeImports = cmd.hasOption("ii");
-        boolean jsonOutput = cmd.hasOption("json");
-        boolean dependencyOutput = cmd.hasOption("do");
+        final Options options = createOptions();
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine cmd;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (final ParseException e) {
+            LOGGER.warning(e.getMessage());
+            printHelp(options);
+            return;
+        }
+
+        final String org = cmd.getOptionValue("o");
+        final String token = cmd.getOptionValue("at");
+        final boolean includeImports = cmd.hasOption("ii");
+        final boolean jsonOutput = cmd.hasOption("json");
+        final boolean dependencyOutput = cmd.hasOption("do");
+        final boolean neo4jOutput = cmd.hasOption("neo4j");
         UpdateSiteTypes updateSiteType = UpdateSiteTypes.NIGHTLY;
-        List<String> reposToIgnore = new ArrayList<>();
-        
+        final List<String> reposToIgnore = new ArrayList<>();
+
+        if (cmd.hasOption("help")) {
+            printHelp(options);
+        }
         if (cmd.hasOption("ur")) {
             updateSiteType = UpdateSiteTypes.RELEASE;
         }
@@ -63,57 +73,66 @@ public class DependencyCLI {
                 while ((line = in.readLine()) != null) {
                     reposToIgnore.add(line);
                 }
-            } catch (IOException e) {
-                LOGGER.warning("Something went wrong while opening the file, please make sure the path is correct.");
+            } catch (final IOException e) {
+                LOGGER.warning("Something went wrong while opening the file, please make sure the path is correct. "
+                        + e.getMessage());
             }
         }
-        
+
         try {
-            GitHubAPIHandler apiHandler = new GitHubAPIHandler(org, token, reposToIgnore);
-            DependencyCalculator dc = new DependencyCalculator(apiHandler, updateSiteType);
-            Set<RepositoryObject> repositories = dc.calculateDependencies(includeImports);
-            GraphicalRepresentation graphRep = new GraphicalRepresentation(repositories);
+            final GitHubAPIHandler apiHandler = new GitHubAPIHandler(org, token, reposToIgnore);
+            final DependencyCalculator dc = new DependencyCalculator(apiHandler, updateSiteType);
+            final Set<RepositoryObject> repositories = dc.calculateDependencies(includeImports);
+            final GraphicalRepresentation graphRep = new GraphicalRepresentation(repositories);
             graphRep.createTopologyHierarchy();
-            List<Set<RepositoryObject>> topology = graphRep.getTopologyHierachy();
-            
-            createOutput(dependencyOutput, jsonOutput, repositories, topology);
+            final List<Set<RepositoryObject>> topology = graphRep.getTopologyHierachy();
+
+            createJsonOutput(dependencyOutput, jsonOutput, repositories, topology);
+            createNeo4jOutput(neo4jOutput, repositories);
         } catch (IOException | ParserConfigurationException | SAXException e) {
-            LOGGER.warning("Please make sure you entered the correct organization and authentication token.");
+            LOGGER.warning("Please make sure you entered the correct organization and authentication token. "
+                    + e.getMessage());
         }
     }
-    
+
     private static Options createOptions() {
-        Options options = new Options();
+        final Options options = new Options();
         options.addRequiredOption("o", "organization", true, "An existing GitHub organization.")
-        .addRequiredOption("at", "authentication-token", true, "Valid authentification token for GitHub API.");
-        options.addOption("ur", "use-release", false, "Use release update site instead of nightly.")
-        .addOption("ii", "include-imports", false, "Consider feature.xml includes while calculating dependencies.")
-        .addOption("json", "json-output", false, "Use more informational json output.")
-        .addOption("do", "dependency-output", false, "Use dependencies per repo output")
-        .addOption("ri", "repository-ignore", true, "Specify one or more repositories which should be ignored when calculating dependencies. Split by an underscore.")
-        .addOption("rif", "repository-ignore-file", true , "Path to file with repositories to ignore. Each repository name must be in a new line.");
+                .addRequiredOption("at", "authentication-token", true, "Valid authentification token for GitHub API.");
+
+        options.addOption("help", "print-help-message", false, "Print this message")
+                .addOption("ur", "use-release", false, "Use release update site instead of nightly.")
+                .addOption("ii", "include-imports", false, "Consider feature.xml includes while calculating dependencies.")
+                .addOption("json", "json-output", false, "Use more informational json output.")
+                .addOption("do", "dependency-output", false, "Use dependencies per repo output")
+                .addOption("ri", "repository-ignore", true, "Specify one or more repositories which should be ignored when calculating dependencies. Split by an underscore.")
+                .addOption("rif", "repository-ignore-file", true, "Path to file with repositories to ignore. Each repository name must be in a new line.")
+                .addOption("neo4j", "create-neo4j-database", false, "Adding the graph representation to a Neo4j graph database.");
+
         return options;
     }
-    
-    private static void createOutput(boolean dependencyOutput, boolean jsonOutput, Set<RepositoryObject> repositories, List<Set<RepositoryObject>> topology) throws JsonProcessingException {
+
+    private static void createJsonOutput(boolean dependencyOutput, boolean jsonOutput,
+            Set<RepositoryObject> repositories, List<Set<RepositoryObject>> topology) throws JsonProcessingException {
         if (dependencyOutput && jsonOutput) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            for (RepositoryObject repo : repositories) {
-                System.out.println(objectMapper.writerWithDefaultPrettyPrinter().withView(Views.Dependency.class).writeValueAsString(repo));
+            final ObjectMapper objectMapper = new ObjectMapper();
+            for (final RepositoryObject repo : repositories) {
+                System.out.println(objectMapper.writerWithDefaultPrettyPrinter().withView(Views.Dependency.class)
+                        .writeValueAsString(repo));
             }
         } else if (dependencyOutput) {
-            for (RepositoryObject repo : repositories) {
-                String dependencyString = "";
-                dependencyString += "Name: " + repo.getRepositoryName() + "\n";
-                dependencyString += "Adress: " + repo.getRepositoryAdress() + "\n";
-                dependencyString += "UpdateSite: " + repo.getUpdateSite() + "\n";
-                dependencyString += "Dependencies: " + repo.getDependencies() + "\n";
-                System.out.println(dependencyString);
+            for (final RepositoryObject repo : repositories) {
+                final StringBuilder dependencyString = new StringBuilder();
+                dependencyString.append("Name: ").append(repo.getRepositoryName()).append("\n");
+                dependencyString.append("Adress: ").append(repo.getRepositoryAdress()).append("\n");
+                dependencyString.append("UpdateSite: ").append(repo.getUpdateSite()).append("\n");
+                dependencyString.append("Dependencies: ").append(repo.getDependencies()).append("\n");
+                System.out.println(dependencyString.toString());
             }
         } else if (jsonOutput) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
-            DefaultPrettyPrinter.Indenter indenter = new DefaultIndenter("  ", DefaultIndenter.SYS_LF);
+            final ObjectMapper objectMapper = new ObjectMapper();
+            final DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
+            final DefaultPrettyPrinter.Indenter indenter = new DefaultIndenter("  ", DefaultIndenter.SYS_LF);
             pp.indentArraysWith(indenter);
             pp.indentObjectsWith(indenter);
             System.out.println(objectMapper.writer(pp).withView(Views.Topology.class).writeValueAsString(topology));
@@ -121,4 +140,26 @@ public class DependencyCLI {
             System.out.println(topology.toString().replaceAll("],", "],\n"));
         }
     }
+
+    private static void createNeo4jOutput(boolean neo4jOutput, Set<RepositoryObject> repositories) {
+        if (neo4jOutput) {
+            try (EmbeddedNeo4j neo4j = new EmbeddedNeo4j()) {
+                neo4j.commit(repositories);
+            } catch (final Exception e) {
+                LOGGER.warning(e.getMessage());
+            }
+        }
+    }
+
+    private static void printHelp(final Options options) {
+        new HelpFormatter().printHelp("java -jar dependencytool.jar <args>", options);
+    }
+
+    /**
+     * Private constructor to avoid object generation.
+     */
+    private DependencyCLI() {
+        throw new IllegalStateException("Utility-class constructor.");
+    }
+
 }
---
+++ src/main/java/org/palladiosimulator/dependencytool/EmbeddedNeo4j.java
@@ -0,0 +1,296 @@
+/*
+ * Copyright 2021 Yves R. Kirschner
+ */
+package org.palladiosimulator.dependencytool;
+
+import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import org.neo4j.dbms.api.DatabaseManagementService;
+import org.neo4j.dbms.api.DatabaseManagementServiceBuilder;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.io.fs.FileUtils;
+
+/**
+ * The Class EmbeddedNeo4j. An object that hold a Neo4j resource until it is
+ * closed.
+ */
+public class EmbeddedNeo4j implements AutoCloseable {
+
+    /**
+     * The Enum Relationships type is mandatory on all relationships and is used to
+     * navigate the graph.
+     */
+    private enum Relationships implements RelationshipType {
+
+        /** The depending on repository relationships. */
+        REPOSITORY("depending on repository"),
+        /** The require the feature relationships. */
+        FEATURE("require the feature"),
+        /** The require the bundle relationships. */
+        BUNDLE("require the bundle");
+
+        /** The relationship message. */
+        private final String message;
+
+        /**
+         * Instantiates a new relationships.
+         *
+         * @param message the message for the relationship
+         */
+        Relationships(String message) {
+            this.message = message;
+        }
+
+        /**
+         * Gets the relationship message.
+         *
+         * @return the message
+         */
+        public String getMessage() {
+            return this.message;
+        }
+    }
+
+    private static final Logger LOGGER = Logger.getLogger(EmbeddedNeo4j.class.getName());
+
+    /**
+     * Registers a new virtual-machine shutdown hook for the database management
+     * service.
+     *
+     * @param managementService the management service to shutdown
+     */
+    private static void addShutdownHook(final DatabaseManagementService managementService) {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                managementService.shutdown();
+            }
+        });
+    }
+
+    /**
+     * Checks if a directory is not empty.
+     *
+     * @param path the path to check
+     * @return true, if is not empty directory
+     */
+    private static boolean isNotEmptyDirectory(Path path) {
+        try {
+            return (Files.isDirectory(path) && !FileUtils.isDirectoryEmpty(path));
+        } catch (final IOException e) {
+            LOGGER.warning(e.getMessage());
+            return false;
+        }
+    }
+
+    /** The path for the Neo4j database directory. */
+    private final Path databaseDirectory;
+
+    /** The Neo4j database service. */
+    private final GraphDatabaseService databaseService;
+
+    /** The Neo4j management service. */
+    private final DatabaseManagementService managementService;
+
+    /** The label for repository nodes. */
+    private final Label repositoryLabel = Label.label("Repository");
+
+    /** The label for feature nodes. */
+    private final Label featureLabel = Label.label("Feature");
+
+    /** The label for bundle nodes. */
+    private final Label bundleLabel = Label.label("Bundle");
+
+    /** The mapping from repository names to their node ids. */
+    private final Map<String, Long> repositoryIds = new HashMap<>();
+
+    /** The mapping from feature names to their node ids. */
+    private final Map<String, Long> featureIds = new HashMap<>();
+
+    /** The mapping from bundle names to their node ids. */
+    private final Map<String, Long> bundleIds = new HashMap<>();
+
+    /**
+     * Instantiates a new embedded Neo4j database instance.
+     */
+    public EmbeddedNeo4j() {
+        this.databaseDirectory = Path.of(DEFAULT_DATABASE_NAME).toAbsolutePath().normalize();
+
+        if (isNotEmptyDirectory(this.databaseDirectory)) {
+            LOGGER.warning("Already found a database. New nodes will be added to it. " + this.databaseDirectory);
+        }
+
+        this.managementService = new DatabaseManagementServiceBuilder(this.databaseDirectory).build();
+        this.databaseService = this.managementService.database(DEFAULT_DATABASE_NAME);
+        addShutdownHook(this.managementService);
+
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (this.managementService != null) {
+            LOGGER.info("Shut down database");
+            this.managementService.shutdown();
+        }
+    }
+
+    /**
+     * Commit the given repository object to the Neo4j database instance.
+     *
+     * @param repositories the repositories to commit
+     */
+    public void commit(Set<RepositoryObject> repositories) {
+        for (final RepositoryObject repository : Objects.requireNonNull(repositories,
+                "The list of repositories must not be null.")) {
+
+            this.commitFeature(repository);
+            this.commitBundle(repository);
+            this.commitRepository(repository);
+        }
+    }
+
+    /**
+     * Commit bundle.
+     *
+     * @param repository the repository
+     */
+    private void commitBundle(final RepositoryObject repository) {
+        for (final String bundle : repository.getRequiredBundles()) {
+            try (Transaction tx = this.databaseService.beginTx()) {
+                final String repositoryName = repository.getRepositoryName();
+                final Node repositoryNode = this.createRepositoryNode(tx, repositoryName);
+
+                final Node bundleNode = this.createBundleNode(tx, bundle);
+
+                this.createRelationship(repositoryNode, bundleNode, Relationships.BUNDLE);
+
+                tx.commit();
+                LOGGER.info(repositoryName + " -> " + bundle);
+            }
+        }
+    }
+
+    /**
+     * Commit feature.
+     *
+     * @param repository the repository
+     */
+    private void commitFeature(final RepositoryObject repository) {
+        for (final String feature : repository.getRequiredFeatures()) {
+            try (Transaction tx = this.databaseService.beginTx()) {
+                final String repositoryName = repository.getRepositoryName();
+                final Node repositoryNode = this.createRepositoryNode(tx, repositoryName);
+
+                final Node featureNode = this.createFeatureNode(tx, feature);
+
+                this.createRelationship(repositoryNode, featureNode, Relationships.FEATURE);
+
+                tx.commit();
+                LOGGER.info(repositoryName + " -> " + feature);
+            }
+        }
+    }
+
+    /**
+     * Commit repository.
+     *
+     * @param repository the repository
+     */
+    private void commitRepository(final RepositoryObject repository) {
+        for (final RepositoryObject dependency : repository.getDependency()) {
+            try (Transaction tx = this.databaseService.beginTx()) {
+                final String repositoryName = repository.getRepositoryName();
+                final Node repositoryNode = this.createRepositoryNode(tx, repositoryName);
+
+                final String dependencyName = dependency.getRepositoryName();
+                final Node dependencyNode = this.createRepositoryNode(tx, dependencyName);
+
+                this.createRelationship(repositoryNode, dependencyNode, Relationships.REPOSITORY);
+
+                tx.commit();
+                LOGGER.info(repositoryName + " -> " + dependencyName);
+            }
+        }
+    }
+
+    /**
+     * Creates the bundle node.
+     *
+     * @param tx     the tx
+     * @param bundle the bundle
+     * @return the node
+     */
+    private Node createBundleNode(Transaction tx, String bundle) {
+        if (this.bundleIds.containsKey(bundle)) {
+            return tx.getNodeById(this.bundleIds.get(bundle));
+        }
+
+        final Node node = tx.createNode();
+        node.addLabel(this.bundleLabel);
+        node.setProperty("name", bundle);
+        this.bundleIds.put(bundle, node.getId());
+        return node;
+    }
+
+    /**
+     * Creates the feature node.
+     *
+     * @param tx      the tx
+     * @param feature the feature
+     * @return the node
+     */
+    private Node createFeatureNode(Transaction tx, String feature) {
+        if (this.featureIds.containsKey(feature)) {
+            return tx.getNodeById(this.featureIds.get(feature));
+        }
+
+        final Node node = tx.createNode();
+        node.addLabel(this.featureLabel);
+        node.setProperty("name", feature);
+        this.featureIds.put(feature, node.getId());
+        return node;
+    }
+
+    /**
+     * Creates the relationship.
+     *
+     * @param from     the from
+     * @param to       the to
+     * @param relation the relation
+     */
+    private void createRelationship(final Node from, final Node to, final Relationships relation) {
+        from.createRelationshipTo(to, relation).setProperty("message", relation.getMessage());
+    }
+
+    /**
+     * Creates the repository node.
+     *
+     * @param tx         the tx
+     * @param repository the repository
+     * @return the node
+     */
+    private Node createRepositoryNode(Transaction tx, final String repository) {
+        if (this.repositoryIds.containsKey(repository)) {
+            return tx.getNodeById(this.repositoryIds.get(repository));
+        }
+
+        final Node node = tx.createNode();
+        node.addLabel(this.repositoryLabel);
+        node.setProperty("name", repository);
+        this.repositoryIds.put(repository, node.getId());
+        return node;
+    }
+}
